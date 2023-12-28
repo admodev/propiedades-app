c18a28c47cab63c7cdd779f2067c23c5a546572e
admodev <adolfohectormoyano@gmail.com>
2023-12-28 12:45:02 -0300

    add properties controller methods, add properties views, translate english words to spanish for the sake of understanding of users through pages

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,5 +4,5 @@ class Property < ApplicationRecord
 
   validates :price, numericality: { major_than: 0 }
   validates :commune, :address, presence: true
-  validates :description, format: { without: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b|\bhttps?:\/\/\S+\b|\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b/, message: "description field should not contain contact data." }
+  validates :description, format: { without: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b|\bhttps?:\/\/\S+\b|\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b/, message: "la descripcion no puede contener informacion de contacto." }
 end
